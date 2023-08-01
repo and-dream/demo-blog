4922ab01075ce4994b5eada5f082a859d7ab7ec5
Gloria <gloriadamase@gmail.com>
2023-08-01 10:24:02 +0200

    redirect

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class BlogController extends AbstractController
 {
@@ -29,7 +32,7 @@ class BlogController extends AbstractController
     {
         //le retour du template
         return $this->render('blog/home.html.twig', [
-          'title' => 'Bienvenue sur mon blog',
+          'title' => 'Bienvenue sur mon blog',         // utiliser des variables sur la page, ça nous permet d'afficher les valeurs
           'age' => 22,  
         ]);  
     }   /*dans le render en 2ème argument on peut envoyer des données dans la vue (twig) sous forme de tableau avec indice = >valeur 
@@ -38,12 +41,40 @@ class BlogController extends AbstractController
     
         // ici on a créé une route et une méthode
         #[Route('/blog/ajout', name:"blog_ajout")]
-        public function form() :Response
+        public function form(Request $globals, EntityManagerInterface $manager) :Response
         {
             $article = new Article();     //on va ratacher cet objet à une entity
             
             //creer un formulaire en utilisant une méthode AbstractController avec 1 argument
-            $form = $this->createForm(ArticleType::class);
+            $form = $this->createForm(ArticleType::class, $article);   //ratacher $article au formulaire
+
+            //on veut récupérer tous les inputs et on veut les vérifier, on va utiliser une méthode vu que notre form est un objet
+            //la variable $globals contient les superglobales
+            //* handleRequest() permet de récupérer toutes les données de mes inputs
+            $form->handleRequest($globals);
+
+            //vérifier si l'utilisateur a cliqué sur Envoyer
+            if($form->isSubmitted()&& $form ->isValid())
+            {
+                //ce qui est en POST c'est la méthode request de l'objet globals
+                // dd($globals->request); 
+                $article->setCreatedAt(new \Datetime);
+                
+                // dd($article);
+
+                //on met en paramètre notre objet $article qui contient les données à envoyer en bdd
+                //*persist() va permettre de préparer ma requête SQL a envoyer par rapport à l'objet donné en argument
+                $manager->persist($article); 
+
+                //* flush va permettre d'exécuter tout les persist précédents
+                $manager->flush();
+
+                //redirection, on va faire un return et utiliser une méthode de AbstractController, il faut lui donner un nom de route
+                //* redirectToRoute() permet de rediriger vers une autre page de  notre site à l'aide du nom de la route (name)
+                return $this->redirectToRoute('home'); 
+
+            }
+
 
             //ici on va render notre page twig
             return $this->render('blog/form.html.twig', [                 
